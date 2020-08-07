2f6c73a766d52defc44056a6cfe06c744fbe8a26
Chislaine Cruz <chislaine.cruz@gmail.com>
2020-08-07 12:27:33 -0400

    added approach to right-shift

--- linked-lists/right-shift.js
+++ linked-lists/right-shift.js
@@ -21,6 +21,23 @@ Constraints
 k >= 0
 */
 
+/*
+Approach: Create a cycle and break it at correct node.
+
+1. Get tail and list size
+2. Create cycle
+3. Handle edge cases
+    1. If k is greater than the length of the list
+        1. Do k % n and thats how many shifts need to be done
+
+4. To get new head: Jump n-k times to from old head
+5. To get to new tail: Jump n-k times from old tail
+
+O(1) space - done in place
+O(n) time - we have to iterate through the entire list to get length and tail
+
+*/
+
 const rotateRight = (head, k) => {
   let tail = head;
   let length = 1;
