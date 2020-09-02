d0fe2393182d6f682bcb38ba24259ea56f4ab32e
Chislaine Cruz <chislaine.cruz@gmail.com>
2020-09-02 13:35:40 -0400

    alternate-string

---
+++ primitives/alternate-characters.js
@@ -0,0 +1,64 @@
+/*
+You are given a string containing characters  and  only. Your task is to change it into a string such that there are no matching adjacent characters. To do this, you are allowed to delete zero or more characters in the string.
+
+Your task is to find the minimum number of required deletions.
+
+For example, given the string , remove an  at positions  and  to make  in  deletions.
+
+Function Description
+
+Complete the alternatingCharacters function in the editor below. It must return an integer representing the minimum number of deletions to make the alternating string.
+
+alternatingCharacters has the following parameter(s):
+
+s: a string
+Input Format
+
+The first line contains an integer , the number of queries.
+The next  lines each contain a string .
+
+Constraints
+
+Each string  will consist only of characters A and B
+Output Format
+
+For each query, print the minimum number of deletions required on a new line.
+
+Sample Input
+
+5
+AAAA
+BBBBB
+ABABABAB
+BABABA
+AAABBB
+Sample Output
+
+3
+4
+0
+0
+4
+Explanation
+
+The characters marked red are the ones that can be deleted so that the string doesn't have matching consecutive characters.
+*/
+
+function alternatingCharacters(s) {
+  let deletions = 0;
+  let string = s[0]; //O(n) space
+  let j = 0;
+
+  for (let i = 1; i < s.length; i++) {
+    //O(n) time
+    if (s[i] === string[j]) {
+      deletions++;
+    } else {
+      string += s[i];
+      j++;
+    }
+  }
+  return deletions;
+}
+
+//O(n) time and space
